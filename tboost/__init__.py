5c17ce1096f02dd0789d51a012545a7bc3982627
Benjamin Cross <btcross26@yahoo.com>
2023-01-14 16:36:36 -0500

    modifying top level init file

--- tboost/__init__.py
+++ tboost/__init__.py
@@ -1,13 +1,12 @@
-"""genestboost top-level module."""
+"""tboost top-level module."""
 
 # author: Benjamin Cross
 # email: btcross26@yahoo.com
-# created: 2019-08-28
+# created: 2023-01-14
 
-from .boosted_linear_model import BoostedLinearModel
 from .boosted_model import BoostedModel
 from .model_data_sets import ModelDataSets
 
-__version__ = "0.3.1"
+__version__ = "0.0.0"
 
-__all__ = ["BoostedLinearModel", "BoostedModel", "ModelDataSets", "__version__"]
+__all__ = ["BoostedModel", "ModelDataSets", "__version__"]
