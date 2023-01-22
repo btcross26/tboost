4efe52d5c36749d82acdf366e1636709068907c7
Benjamin Cross <btcross26@yahoo.com>
2023-01-22 08:01:30 -0500

    add sigmoid invertible layer

--- tboost/torch_modules/__init__.py
+++ tboost/torch_modules/__init__.py
@@ -6,10 +6,12 @@
 
 from .linear_invertible_layer import LinearInvertible
 from .relu_invertible_layer import LeakyReLUInvertible
+from .sigmoid_invertible import SigmoidInvertible
 from .view_layer import View
 
 __all__ = [
     "LeakyReLUInvertible",
     "LinearInvertible",
+    "SigmoidInvertible",
     "View",
 ]
